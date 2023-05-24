9a20ab5bfaca394114acfd6fa3c39835d503d98f
Dharmil Shah <dharmilshah476@gmail.com>
2023-05-25 01:37:07 +0530

    Added Google Line Chart

--- src/components/dashboard/activities/Activities.js
+++ src/components/dashboard/activities/Activities.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./activities.css";
+import Chart from 'react-google-charts'
+
+const LineData = [
+  ['x', 'Guest', 'User'],
+  [0, 0, 0],
+  [1, 10, 5],
+  [2, 23, 15],
+  [3, 17, 9],
+  [4, 18, 10],
+  [5, 9, 5],
+  [6, 11, 3],
+  [7, 27, 19],
+]
+const LineChartOptions = {
+  hAxis: {
+    title: 'Time',
+  },
+  vAxis: {
+    title: 'Popularity',
+  },
+  series: {
+    1: { curveType: 'function' },
+  },
+}
 
 function Activities() {
   return (
-    <div>Activities</div>
-  )
+    <div className="activities-container">
+      <h2>Activities</h2>
+        <Chart
+          width={'100%'}
+          height={'500px'}
+          chartType="LineChart"
+          loader={<div>Loading Chart</div>}
+          data={LineData}
+          options={LineChartOptions}
+          rootProps={{ 'data-testid': '2' }}
+        />
+    </div>
+  );
 }
 
-export default Activities
+export default Activities;
