358e1e6fa32721f873891d0224f4f6724a46f3c8
Dharmil Shah <dharmilshah476@gmail.com>
2023-05-24 23:49:22 +0530

    Navbar

--- src/components/dashboard/navbar/Navbar.js
+++ src/components/dashboard/navbar/Navbar.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
+import Bell from '../../../assets/bell.png'
 function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="navbar-left">
+        <h1>Dashboard</h1>
+      </div>
+      <div className="navbar-right">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+        </div>
+        <div className="notification-bell">
+            <img src={Bell} alt="" />
+        </div>
+        <div className="profile">
+          <div className="profile-img">
+            <img
+              src="https://images.pexels.com/photos/2586823/pexels-photo-2586823.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
